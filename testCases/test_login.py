213220409bb72f5bb2fb8d521f8ccca4d1104d4f
Sateesh kumar <sateeshkumar0315@gmail.com>
2021-09-27 12:17:56 +0530

    "Readproperties.py adn Config.ini files were added"

--- testCases/test_login.py
+++ testCases/test_login.py
@@ -1,10 +1,5 @@
-import pytest, os
-from selenium import webdriver
 from pageObject.Loginpage import Login
-from main import file_path
-import selenium
 from utilities.readproperties import ReadConfig
-import os
 
 
 class TestLoginPage:
@@ -16,11 +11,11 @@ class TestLoginPage:
         self.driver = setup
         self.driver.get(self.base_url)
         act_title = self.driver.title
-        self.driver.close()
         if act_title == 'Your store. Login':
             assert True
             self.driver.close()
         else:
+            self.driver.save_screenshot('E:/Videos/Courses/Practice/GIT/automation-framework/screenshots/error.png')
             self.driver.close()
             assert False
 
@@ -31,20 +26,11 @@ class TestLoginPage:
         self.lp.setup_username(self.username)
         self.lp.setup_password(self.password)
         self.lp.click_login()
-        login_page_title = self.driver.title
         db_title = self.driver.title
         if db_title == "Dashboard / nopCommerce administration":
             print("\nStatus is True")
             assert True
-            self.driver.save_screenshot("E:\Videos\Courses\Practice\GIT\automation-framework\screenshots"+"scrsht.png")
+            self.driver.save_screenshot("E:/Videos/Courses/Practice/GIT/automation-framework/screenshots/Dashboard.png")
             self.driver.close()
-
         else:
             assert False
-
-    def test_logout(self, setup):
-        self.driver = setup
-        self.test_login()
-        self.lp = Login()
-        self.lp.click_logout()
-        self.driver.close()
--- utilities/readproperties.py
+++ utilities/readproperties.py
@@ -1,23 +1,21 @@
 import configparser
-import os
-
 
 config = configparser.RawConfigParser()
-config.read("E:\Videos\Courses\Practice\GIT\automation-framework\configurations\config.ini")
+config.read("E:/Videos/Courses/Practice/GIT/automation-framework/configurations/config.ini")
 
 
 class ReadConfig:
     @staticmethod
     def base_url():
-        url = config.get('common info', 'baseURL')
-        return url
+        base_url = config.get('common info', 'baseURL')
+        return base_url
 
     @staticmethod
     def get_username():
-        username = config.get("common info", "username")
+        username = config.get('common info', 'username')
         return username
 
     @staticmethod
     def get_password():
-        password = config.get("common info", "password")
+        password = config.get('common info', 'password')
         return password
