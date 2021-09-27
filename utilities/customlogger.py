d0cfdd1702bb133f8ac1ee17a77aa0a09259e007
Sateesh kumar <sateeshkumar0315@gmail.com>
2021-09-27 21:15:00 +0530

    "Log file Created and Updated Test cases"

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -4,10 +4,13 @@ import logging
 class LogGen:
     @staticmethod
     def log_gen():
-        logging.basicConfig(filename='E:/Videos/Courses/Practice/GIT/automation-framework/logs/TestLogs.log',
+        logging.basicConfig(filename='E:/Videos/Courses/Practice/GIT/automation-framework/logs/logs.log',
                             format='%(asctime)s: %(levelname)s: %(message)s',
                             datefmt='%d/%m/%Y %I:%M:%S %p',
                             )
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
         return logger
+
+
+LogGen.log_gen()
