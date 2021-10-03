568036ffafdee68c8063135c416d424d61f86762
Sateesh kumar <sateeshkumar0315@gmail.com>
2021-10-03 23:30:00 +0530

    "Log File Path Updated"

--- utilities/customlogger.py
+++ utilities/customlogger.py
@@ -5,8 +5,8 @@ import os
 class LogGen:
     @staticmethod
     def log_gen():
-        file_name = os.path.abspath(os.path.dirname(".\\..\logs\."))
-        logging.basicConfig(filename=file_name + "\logs.log",
+        file_path = os.path.abspath(os.path.dirname(".\\..\logs\."))
+        logging.basicConfig(filename=file_path + "\logs.log",
                             format='%(asctime)s: %(levelname)s: %(message)s',
                             datefmt='%d/%m/%Y %I:%M:%S %p'
                             )
